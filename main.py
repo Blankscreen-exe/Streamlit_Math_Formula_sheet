de73c28f8ea257b596dca5ec980ef62ff21240c2
Muhammad Hammad Hassan <51869983+Blankscreen-exe@users.noreply.github.com>
2022-06-21 10:53:37 +0000

    update main.py

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 import streamlit as st
-from formula.relativity import relativity as REL
+
+import RelativityApp as RELapp
+# import Euler as EULapp
+# import EulerIdentitiy as EULIDapp
+# import Quadratic as QUAapp
+# import QuadraticEq as QUAEQapp
 
 st.write("# Math Formula App")
+options = ["Relativity","Euler Identity","Quadratic Equation","Quadratic"]
+app = st.selectbox("Select a Formula to run", options)
 
-st.write("Under Development")
-st.write(REL(3,4))
+if app == options[0]:
+  try:
+    RELapp.RelativityApp()
+  except:
+    st.error("Something went wrong in Relativity App")
+elif app == options[1]:
+  try:
+    EULIDapp.EulerIdentityApp()
+  except:
+    st.error("Something went wrong in Euler identity App")
